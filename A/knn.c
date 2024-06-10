20ca87d554c4a228469fb734771242a0b45bb260
gacrescencio <gacrescencio@gmail.com>
2024-06-09 23:46:42 -0300

    paralelizacao do knn

--- A/knn.c
+++ A/knn.c
@@ -10,29 +10,32 @@ char knn(int n_groups, Group * groups, int k, Point to_evaluate) {
         labels[i] = -1;
         distances[i] = -1;
     }
-
+    //Paraleliza o loop que percorre todos os grupos e mede as distâncias
+    #pragma omp parallel for private(i, j, x, y) shared(labels, distances)
     for (i = 0; i < n_groups; i++) {
         Group g = groups[i];
-
         for (j = 0; j < g.length; j++) {
             float d = euclidean_distance_no_sqrt(to_evaluate, g.points[j]);
-
+            // utilizado o omp critical para evitar condição de corrida, uma vez que as variaveis diastances e labels são compartilhadas
+            #pragma omp critical
+            {
+            // Insere a nova distância e rótulo na posição correta nos arrays
             for (x = 0; x < k; x++) {
                 if (d < distances[x] || distances[x] == -1) {
                     for (y = k - 1; y > x; y--) {
                         distances[y] = distances[y - 1];
                         labels[y] = labels[y - 1];
                     }
-
                     distances[x] = d;
                     labels[x] = g.label;
-
                     break;
                 }
             }
         }
+        
     }
-
+    }
+    
     qsort(labels, k, sizeof(char), compare_for_sort);
 
     char most_frequent = labels[0];
@@ -66,6 +69,8 @@ int main() {
     
     Group * groups = (Group *) malloc(sizeof(Group) * n_groups);
 
+    // Paraleliza a leitura e inicialização dos grupos
+    #pragma omp parallel for
     for (int i = 0; i < n_groups; i++) {
         groups[i] = parse_next_group();
     }
